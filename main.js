ddd34ae609d6f311b0f66a1be589d3ba33258c17
YuukiAnimeSensei <icchan417@gmail.com>
2022-05-07 14:03:50 +0900

    湯布院追加

--- main.js
+++ main.js
@@ -21,8 +21,37 @@ const mapOptions = {
 loader
   .load()
   .then((google) => {
-    new google.maps.Map(document.getElementById("app"), mapOptions);
+    initMap();
+    //new google.maps.Map(document.getElementById("app"), mapOptions);
   })
   .catch(e => {
     // do something
   });
+// This example creates a 2-pixel-wide red polyline showing the path of
+// the first trans-Pacific flight between Oakland, CA, and Brisbane,
+// Australia which was made by Charles Kingsford Smith.
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("app"), {
+    zoom: 3,
+    center: { lat: 0, lng: -180 },
+    mapTypeId: "terrain",
+  });
+  const flightPlanCoordinates = [
+    { lat:33.295409137691024, lng:131.35685016661338},
+    { lat: 37.772, lng: -122.214 },
+    { lat: 21.291, lng: -157.821 },
+    { lat: -18.142, lng: 178.431 },
+    { lat: -27.467, lng: 153.027 },
+  ];
+  const flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: "#00FF00",
+    strokeOpacity: 0.7,
+    strokeWeight: 5,
+  });
+
+  flightPath.setMap(map);
+}
+
+window.initMap = initMap;
